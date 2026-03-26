7fc0974ebe2b72158006a1571c78efaecdd39619
Vinícius Vidal <viniciusvidal@Mac.home>
2026-03-26 01:58:44 -0300

    codecrafters submit [skip ci]

--- src/command.c
+++ src/command.c
@@ -48,7 +48,7 @@ void cmd_type(String_View args) {
         return;
     }
 
-    printf(SV_FMT ": command not found\n", SV_ARG(args));
+    printf(SV_FMT ": not found\n", SV_ARG(args));
 }
 
 void command_dispatch(String_View cmd, String_View args) {
