a2257cf437abb3844b16819f88aece3fcf2ae74c
Vinícius Vidal <viniciusvidal@Mac.home>
2026-03-26 00:23:02 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
-  // printf("$ ");
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
 
-  return 0;
+    return 0;
 }
