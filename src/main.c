0c292bd777da3945e24a493584eeebc632080db8
Vinícius Vidal <viniciusvidal@Mac.home>
2026-03-26 00:53:57 -0300

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/syslimits.h>
+#include <unistd.h>
+
+#define BUF_SIZE 256
 
 int main(int argc, char *argv[]) {
     // Flush after every printf
     setbuf(stdout, NULL);
 
-    // TODO: Uncomment the code below to pass the first stage
-    printf("$ ");
+    while (1) {
+        char cwd[PATH_MAX];
+        if (getcwd(cwd, sizeof(cwd)) == NULL) {
+            perror("getcwd");
+        };
+
+        printf("%s\n", cwd);
+        printf("λ ");
+        char buf[BUF_SIZE];
+        if (fgets(buf, sizeof(buf), stdin) == NULL) {
+            if (feof(stdin)) {
+                printf("\n");
+                return EXIT_SUCCESS;
+            }
+
+            perror("fgets");
+            return EXIT_FAILURE;
+        }
+        buf[strcspn(buf, "\n")] = '\0';
+
+        printf("%s: command not found\n\n", buf);
+    }
 
     return 0;
 }
