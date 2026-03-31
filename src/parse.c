a9b88a133d57752bdc522c80778d2b4e0926d29b
Vinícius Vidal <viniciusvidal@Viniciuss-MacBook-Pro.local>
2026-03-31 14:41:32 -0300

    codecrafters submit [skip ci]

--- src/parse.c
+++ src/parse.c
@@ -1,44 +1,57 @@
+#include <assert.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stddef.h>
 
+typedef struct {
+    char *base;
+    char *ptr;
+    char *end;
+} Arena;
+
+void *arena_alloc(Arena *a, size_t size) {
+    assert(a->ptr + size <= a->end);
+    void *p = a->ptr;
+    a->ptr += size;
+    return p;
+}
+
+void arena_reset(Arena *a) { a->ptr = a->base; }
+
 size_t parse_args(const char *input, char *storage, char **argv) {
     size_t argc = 0;
-    size_t storage_offset = 0;
-    size_t token_len = 0;
-    bool in_single_quotes = false;
-    bool in_double_quotes = false;
+    char *out = storage;
+    char *token_start = NULL;
+    bool in_single = false, in_double = false;
 
-    for (const char *p = input; *p != '\0'; p++) {
+    for (const char *p = input; *p; p++) {
         char c = *p;
 
-        if (c == '\'' && !in_double_quotes) {
-            in_single_quotes = !in_single_quotes;
+        if (c == '\'' && !in_double) {
+            in_single = !in_single;
+            continue;
+        }
+        if (c == '"' && !in_single) {
+            in_double = !in_double;
             continue;
         }
 
-        if (c == '"' && !in_single_quotes) {
-            in_double_quotes = !in_double_quotes;
-            continue;
-        }
+        if (in_single || in_double || !isspace(c)) {
+            if (!token_start) {
+                token_start = out;
+            }
 
-        bool in_quotes = in_single_quotes || in_double_quotes;
-
-        if (in_quotes || !isspace(c)) {
-            storage[storage_offset + token_len++] = c;
-        } else if (token_len > 0) {
-            storage[storage_offset + token_len] = '\0';
-            argv[argc++] = &storage[storage_offset];
-            storage_offset += token_len + 1;
-            token_len = 0;
+            *out++ = c;
+        } else if (token_start) {
+            *out++ = '\0';
+            argv[argc++] = token_start;
+            token_start = NULL;
         }
     }
-
-    if (token_len > 0) {
-        storage[storage_offset + token_len] = '\0';
-        argv[argc++] = &storage[storage_offset];
+    if (token_start) {
+        *out = '\0';
+        argv[argc++] = token_start;
     }
-
     argv[argc] = NULL;
     return argc;
 }
